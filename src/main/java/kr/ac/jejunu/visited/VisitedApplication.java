f9e3fbd2a5419f2f61cdb37920518fca4d96b617
qww1552 <qww1552>
2021-06-04 20:44:25 +0900

    first commit

---
+++ src/main/java/kr/ac/jejunu/visited/VisitedApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.visited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/jejunu/visited/VisitedApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.visited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
