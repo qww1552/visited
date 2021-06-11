3197d4bc8c887fcb782a7dab82899d661c6d668e
qww1552 <qww1552>
2021-06-11 15:14:28 +0900

    카드가 존재하지 않을 경우 예외처리

--- src/main/java/kr/ac/jejunu/visited/controller/CardController.java
+++ src/main/java/kr/ac/jejunu/visited/controller/CardController.java
@@ -9,27 +9,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("cards")
-@CrossOrigin(value = "http://localhost:5000/", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+@CrossOrigin(value = "http://localhost:5000/", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class CardController {
     @Autowired
     private final CardRepository cardRepository;
 
     @GetMapping
-    public List<Card> getAllCards() {
-        return cardRepository.findAll();
+    public ResponseEntity getAllCards() {
+        List<CardDto> cards = new LinkedList<>();
+        for (Card card : cardRepository.findAll()) {
+            cards.add(new CardDto(card));
+        }
+        return new ResponseEntity(cards, HttpStatus.OK);
     }
 
     @GetMapping("/{cardId}")
     public ResponseEntity getOneCard(@PathVariable Integer cardId) {
         CardDto card = new CardDto(
                 cardRepository.findById(cardId).orElseThrow(() ->
-                new NoSuchElementException("no card"))
+                        new NoSuchElementException("card not found"))
         );
         return new ResponseEntity(card, HttpStatus.OK);
     }
@@ -37,13 +42,15 @@ public class CardController {
     @PostMapping
     public ResponseEntity addCard(@RequestBody Card newCard) {
         CardDto save = new CardDto(cardRepository.save(newCard));
-        return new ResponseEntity(save,HttpStatus.CREATED);
+        return new ResponseEntity(save, HttpStatus.CREATED);
     }
 
     @PutMapping("/{cardId}")
     public ResponseEntity updateCard(@PathVariable Integer cardId, @RequestBody Card update) {
         // 널일 경우 처리 필요
-
+        cardRepository.findById(cardId).orElseThrow(() ->
+                new NoSuchElementException("card not found")
+        );
         Card card = Card.builder()
                 .id(cardId)
                 .author(update.getAuthor())
@@ -54,15 +61,21 @@ public class CardController {
                 .build();
 
         CardDto updated = new CardDto(cardRepository.save(card));
-        return new ResponseEntity(updated,HttpStatus.OK);
+        return new ResponseEntity(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/{cardId}")
     public ResponseEntity deleteCard(@PathVariable Integer cardId) {
+        cardRepository.findById(cardId).orElseThrow(() ->
+                new NoSuchElementException("card not found")
+        );
         cardRepository.deleteById(cardId);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity handleNoSuchElementException(Exception e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
 }
